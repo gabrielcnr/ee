9888c8a64fff404865419e77b099f0169bce4945
Marcelo Trevisani <marceloduartetrevisani@gmail.com>
2021-12-14 19:56:02 +0000

    Add setuptools-scm

--- setup.py
+++ setup.py
@@ -6,6 +6,10 @@ setup(
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     zip_safe=False,
+    use_scm_version={"write_to": "src/ee/_version.py"},
+    setup_requires=["setuptools-scm", "setuptools>=30.3.0"],
+    python_requires=">=3.8",
+    extras_require={"testing": ["pytest", "pytest-mock", "responses"]},
     entry_points={
         "console_scripts": [
             "ee-run = ee.cli:app",
