3624518d188935a3009efd2cf0f48d8a64c59c43
Gabriel Reis <gabrielcnr@gmail.com>
2022-02-10 20:20:42 +0000

    FIX: mocked calls checks

--- tests/test_backends/test_conda_deployment_backend.py
+++ tests/test_backends/test_conda_deployment_backend.py
@@ -20,9 +20,6 @@ def test_conda_deployment_backend_run_command(mocker, in_memory_store):
     mock_subprocess_run = mocker.patch(
         "ee.backends.conda_deployment_backend.subprocess.run", autospec=True
     )
-    mock_subprocess_Popen = mocker.patch(
-        "ee.backends.conda_deployment_backend.subprocess.Popen", autospec=True
-    )
 
     service.run("my-app", "prod", ["foo", "bar"])
 
@@ -39,14 +36,9 @@ def test_conda_deployment_backend_run_command(mocker, in_memory_store):
             shell=SHELL,
             capture_output=True,
         ),
+        mocker.call(
+            "conda run --no-capture-output -n base "
+            "conda run --no-capture-output -n 412b992 foo bar".split(),
+            shell=SHELL,
+        ),
     ]
-
-    mock_subprocess_Popen.assert_has_calls(
-        [
-            mocker.call(
-                "conda run --no-capture-output -n base "
-                "conda run --no-capture-output -n 412b992 foo bar".split(),
-                shell=SHELL,
-            ),
-        ]
-    )
